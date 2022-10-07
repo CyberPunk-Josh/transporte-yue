f92d6944bf122becf92e13dec74ddea55c538005
Joshue <63613514+CyberPunk-Josh@users.noreply.github.com>
2022-10-07 15:43:07 -0500

    Update config.js
    variables edit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -14,15 +14,6 @@ const firebaseConfig = {
   appId: `${process.env.REACT_APP_APPID}`
 };
 
-/* const firebaseConfig = {
-  apiKey: "AIzaSyAILbQNdiPOqlqs707YrTM8oxzPAqdgkn4",
-  authDomain: "transporte-yue.firebaseapp.com",
-  projectId: "transporte-yue",
-  storageBucket: "transporte-yue.appspot.com",
-  messagingSenderId: "665575426733",
-  appId: "1:665575426733:web:d7abd9275dad2238385424"
-}; */
-
 // Initialize Firebase
 export const Firebaseapp = initializeApp(firebaseConfig);
 export const FirebaseAuth = getAuth(Firebaseapp);
